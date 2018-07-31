3a163fda33e3a4e4f5f211444f033628a290a032
kleberbaum <kleberbaum@erebos.xyz>
2018-07-31 17:08:33 +0200

    removed all trailing whitespaces

--- adminer/4.6.3/run.sh
+++ adminer/4.6.3/run.sh
@@ -10,10 +10,10 @@ cat > /etc/php7/conf.d/00_custom.ini <<EOF
 EOF
 
 cat <<- EOF > /etc/php7/conf.d/zzz_custom.ini
-    max_execution_time = 0
-    upload_max_filesize = 2000M
-    post_max_size = 2000M
-    memory_limit = -1
+	max_execution_time = 0
+	upload_max_filesize = 2000M
+	post_max_size = 2000M
+	memory_limit = -1
 EOF
 
 # execute CMD[]
